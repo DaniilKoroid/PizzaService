7ee990ff6c94937f75ca50e1d564be8033f28dfe
Daniil_Koroid <Daniil_Koroid@epam.com>
2016-04-05 15:47:49 +0300

    Added equals(), hashCode() to Address, Customer.

--- src/main/java/ua/rd/pizzaservice/domain/address/Address.java
+++ src/main/java/ua/rd/pizzaservice/domain/address/Address.java
@@ -54,4 +54,47 @@ public class Address {
 		return "Address [country=" + country + ", city=" + city + ", street=" + street + ", building=" + building + "]";
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((building == null) ? 0 : building.hashCode());
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + ((country == null) ? 0 : country.hashCode());
+		result = prime * result + ((street == null) ? 0 : street.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Address other = (Address) obj;
+		if (building == null) {
+			if (other.building != null)
+				return false;
+		} else if (!building.equals(other.building))
+			return false;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (country == null) {
+			if (other.country != null)
+				return false;
+		} else if (!country.equals(other.country))
+			return false;
+		if (street == null) {
+			if (other.street != null)
+				return false;
+		} else if (!street.equals(other.street))
+			return false;
+		return true;
+	}
+
 }
