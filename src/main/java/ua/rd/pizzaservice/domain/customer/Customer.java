6d75b3542512c4bc6365ddc2f6c0cdc5e7dfc53c
Daniil_Koroid <Daniil_Koroid@epam.com>
2016-04-23 12:15:01 +0300

    Updated Customer class.

--- src/main/java/ua/rd/pizzaservice/domain/customer/Customer.java
+++ src/main/java/ua/rd/pizzaservice/domain/customer/Customer.java
@@ -1,5 +1,8 @@
 package ua.rd.pizzaservice.domain.customer;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -9,97 +12,98 @@ import ua.rd.pizzaservice.domain.address.Address;
 @Scope("prototype")
 public class Customer {
 
-    private static int idCounter = 0;
+	private Integer id;
 
-    private Integer    id;
+	private String name;
 
-    private String     name;
+	private Set<Address> addresses;
 
-    private Address    address;
+	public Customer() {
+		addresses = new HashSet<>();
+	}
 
-    public Customer() {
-        id = ++idCounter;
-    }
+	public Customer(String name) {
+		this();
+		this.name = name;
+	}
 
-    public Customer(String name) {
-        id = ++idCounter;
-        this.name = name;
-    }
+	public Integer getId() {
+		return id;
+	}
 
-    public Integer getId() {
-        return id;
-    }
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public Set<Address> getAddresses() {
+		return addresses;
+	}
 
-    public Address getAddress() {
-        return address;
-    }
+	public void setAddresses(Set<Address> addresses) {
+		this.addresses = addresses;
+	}
 
-    public void setAddress(Address address) {
-        this.address = address;
-    }
+	public Boolean addAddress(Address address) {
+		return addresses.add(address);
+	}
+	
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", name=" + name + ", addresses=" + addresses + "]";
+	}
 
-    @Override
-    public String toString() {
-        return "Customer [id=" + id + ", name=" + name + ", address=" + address
-                + "]";
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((addresses == null) ? 0 : addresses.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((address == null) ? 0 : address.hashCode());
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        Customer other = (Customer) obj;
-        if (address == null) {
-            if (other.address != null) {
-                return false;
-            }
-        } else if (!address.equals(other.address)) {
-            return false;
-        }
-        if (id == null) {
-            if (other.id != null) {
-                return false;
-            }
-        } else if (!id.equals(other.id)) {
-            return false;
-        }
-        if (name == null) {
-            if (other.name != null) {
-                return false;
-            }
-        } else if (!name.equals(other.name)) {
-            return false;
-        }
-        return true;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Customer other = (Customer) obj;
+		if (addresses == null) {
+			if (other.addresses != null) {
+				return false;
+			}
+		} else if (!addresses.equals(other.addresses)) {
+			return false;
+		}
+		if (id == null) {
+			if (other.id != null) {
+				return false;
+			}
+		} else if (!id.equals(other.id)) {
+			return false;
+		}
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!name.equals(other.name)) {
+			return false;
+		}
+		return true;
+	}
 
 }
