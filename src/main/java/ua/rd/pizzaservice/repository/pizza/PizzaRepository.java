1321acfffcf2dc14fefe598003ae335f80b136ac
Daniil_Koroid <Daniil_Koroid@epam.com>
2016-04-25 11:28:51 +0300

    Changed PizzaRepository interface.

--- src/main/java/ua/rd/pizzaservice/repository/pizza/PizzaRepository.java
+++ src/main/java/ua/rd/pizzaservice/repository/pizza/PizzaRepository.java
@@ -1,8 +1,18 @@
 package ua.rd.pizzaservice.repository.pizza;
 
+import java.util.List;
+
 import ua.rd.pizzaservice.domain.pizza.Pizza;
 
 public interface PizzaRepository {
 
+	void add(Pizza pizza);
+
 	Pizza getPizzaByID(Integer id);
+
+	List<Pizza> getAllPizzas();
+
+	void update(Pizza pizza);
+
+	void delete(Pizza pizza);
 }
